294196d3684ecb65ce84c9d352fb25c655a3493e
Yuta Haga <yutahaga7@gmail.com>
2017-08-21 20:49:48 +0900

    improve method to get the response

--- includes/wp-rest-api-frontpage-v2.php
+++ includes/wp-rest-api-frontpage-v2.php
@@ -70,36 +70,31 @@ if ( ! class_exists( 'WP_REST_API_frontpage' ) ) :
      */
     public static function get_frontpage() {
 
-      $rest_url = trailingslashit( get_rest_url() . self::get_plugin_namespace() . '/frontpage/' );
-
       // Get the ID of the static frontpage. If not set it's 0
       $page_id = get_option('page_on_front');
 
       // If the Frontpage is set, it's id shouldn't be 0
       if ( $page_id > 0 ) {
 
-        // Set url for call to retrieve the post, need WP REST API for this
-        $endpoint = get_site_url() . '/index.php/wp-json/wp/v2/pages/' . $page_id;
+        // Create a request to get the frontpage
+        $request = new WP_REST_Request( 'GET', '/wp/v2/pages/' . $page_id );
 
-        // Do the actual call to retrieve the pageObject by ID
-        $response = file_get_contents($endpoint);
-
-        // Decode the output
-        $output = json_decode($response);
+				// Process the request and get the response
+				$response = rest_do_request( $request );
 
       } else {
-        $output = null;
+        $response = null;
       }
 
       // No static frontpage is set
-      if( empty($output) ) {
-        return new WP_Error( 'wpse-error', 
-          esc_html__( 'No Static Frontpage set', 'wpse' ), 
+      if( empty($response) ) {
+        return new WP_Error( 'wpse-error',
+          esc_html__( 'No Static Frontpage set', 'wpse' ),
           [ 'status' => 404 ] );
       }
 
-      // Response setup
-      return new WP_REST_Response( $output, 200 );
+      // Return the response
+      return $response;
     }
   }
 
